682f3eabbf6717d2575f0536e38e77e6425ceae4
nirbhay266 <153939270+nirbhay266@users.noreply.github.com>
2024-04-13 10:00:36 +0530

    added star1

---
+++ java/star1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class star1 {
+    public static void hollow_ractangle(int totRows,int totCols){
+        for(int i = 1; i<=totRows; i++)
+        {
+            for(int j=1;j<=totCols; j++)
+            {
+                if(i==1 || i==totRows || j==1 || j==totCols)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[] args) {
+        hollow_ractangle(4, 05);
+    }
+}     
+
