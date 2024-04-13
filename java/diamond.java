d81044872b080c436fe4ca40c6c867ed2a462138
nirbhay266 <153939270+nirbhay266@users.noreply.github.com>
2024-04-13 13:00:57 +0530

    added diamond

---
+++ java/diamond.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class diamond {
+    public static void Daimond(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int m = sc.nextInt();
+        Daimond(m);
+        
+    }
+}
