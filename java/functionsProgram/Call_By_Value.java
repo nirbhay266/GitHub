fd1ce26185f01244373517765831d64f6b0421a5
nirbhay266 <153939270+nirbhay266@users.noreply.github.com>
2024-04-13 13:17:45 +0530

    added call_By_Value

---
+++ java/functionsProgram/Call_By_Value.java
@@ -0,0 +1,15 @@
+
+
+public class Call_By_Value {
+    public static void CallByValue(int a,int b)
+    {
+        a=a-b;
+        b=a+b;
+        a=b-a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    public static void main(String[] args) {
+        CallByValue(5, 10);
+    }
+}
