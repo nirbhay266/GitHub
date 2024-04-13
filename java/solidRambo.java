60e5c93d21e857210d331abef33c6f7ea6d04dab
nirbhay266 <153939270+nirbhay266@users.noreply.github.com>
2024-04-13 12:32:35 +0530

    added solidRambo

---
+++ java/solidRambo.java
@@ -0,0 +1,22 @@
+
+
+public class solidRambo {
+    public static void Rambo_Solid(int n)
+    {
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+         Rambo_Solid(5);
+    }
+}
